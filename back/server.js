9da988afda79c82165d40c71be07e5f140d0ed47
Encheng Zhou <ezhou@c2r5s2.42malaga.com>
2024-11-22 17:49:42 +0100

    Half-working Backend

--- back/server.js
+++ back/server.js
@@ -11,6 +11,9 @@ const genAI = new GoogleGenerativeAI(accessKey);
 
 const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
 
+const prompt = "Write a story about a magic backpack.";
 
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
 
 app.listen(5000);
